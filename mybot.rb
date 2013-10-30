03de8dde4478a335dafa22b23ab4a30951d52780
Katrina Verey <kn.verey@gmail.com>
2013-10-30 13:39:09 -0400

    Started pulling weather data

--- mybot.rb
+++ mybot.rb
@@ -1,9 +1,11 @@
 require "socket"
+require 'net/http'
+require 'curb'
+require "json"
 
 class Bot
 
   def initialize
-    @server = "chat.freenode.net"
     @port = "6667"
     @nick = "Katbot"
     @channel = "#bitmakerlabs"
@@ -12,32 +14,55 @@ class Bot
   def run
     connect
     until @server.eof? do
-      msg = @server.gets.downcase
-      pong if ping?
-      respond if relevant?(msg)
+      msg = @server.gets
+      pong(msg) if msg.include? "PING"
+      respond if relevant?(msg.downcase)
     end
   end
 
+  def pong(msg)
+    @server.puts msg.gsub("PING", "PONG")
+  end
+
   def connect
-    irc_server = TCPSocket.open(@server, @port)
-    irc_server.puts "USER #{@nick} 0 * #{@nick}"
-    irc_server.puts "NICK #{@nick}"
-    irc_server.puts "JOIN #{@channel}"
-    #irc_server.puts "PRIVMSG #{@channel} :Message!" #intro greeting
+    @server = TCPSocket.open("chat.freenode.net", @port)
+    @server.puts "USER #{@nick} 0 * #{@nick}"
+    @server.puts "NICK #{@nick}"
+    @server.puts "JOIN #{@channel}"
+    #@server.puts "PRIVMSG #{@channel} :Message!" #intro greeting
   end
 
   def relevant?(msg)
     puts msg
     return false unless msg.include? "privmsg #{@channel} :"
     
-    listening_for = ["test","testing"]
+    listening_for = ["cat"]
     listening_for.each do |keyword|
-      return true if listening_for.include? keyword
+      return true if msg.include? keyword
     end
+    return false
   end
 
   def respond
-    response = "Hi! I work!"
-    irc_server.puts "PRIVMSG #{@channel} :#{response}"
+    response = "Hi!"
+    @server.puts "PRIVMSG #{@channel} :#{response}"
   end
+
+  def get_current_weather(city)
+    url = "http://api.wunderground.com/api/ca74b375a5f317d5/conditions/q/Canada/#{city}.json"
+    data = (JSON.parse Curl.get(url).body_str)["current_observation"]
+    temperature = data["temp_c"]
+    conditions = data["weather"].downcase
+    feels_like = data["feelslike_c"]
+
+    puts "It's #{temperature}°C in #{city} right now. Conditions are #{conditions} and it feels like #{feels_like}°C."
+  end
+
+  def get_forecast(city)
+    url = "http://api.wunderground.com/api/ca74b375a5f317d5/forecast/q/Canada/#{city}.json"
+    data = JSON.parse Curl.get(url).body_str
+  end
+
 end
+
+Bot.new.get_current_weather("Ottawa")
